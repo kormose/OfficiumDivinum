210cc7c27ccdb7d7cd0738cf008a7b0c2cabe921
John Maximilian <2e0byo@gmail.com>
2021-01-29 21:54:50 +0000

    path

--- backend/api/test_api.py
+++ backend/api/test_api.py
@@ -2,11 +2,11 @@ from pathlib import Path
 
 from .api import api
 
-testpage = Path("./backend/api/test.html")
+testpage = Path("./backend/test.html")
 
 
 @api.route("/")
 def test_api_page():
     with testpage.open() as f:
-        data = testpage.read()
+        data = f.read()
     return data
