03d197d757545f54af7552f3719410ef0650e6a5
John Maximilian <2e0byo@gmail.com>
2021-01-29 20:02:43 +0000

    update server library

---
+++ backend/api/update_server.py
@@ -0,0 +1,86 @@
+"""
+Re-used from.
+
+https://github.com/SwagLyrics/swaglyrics-backend/blob/35d23d0ba416e742e381da931d592ce6f58fc13f/issue_maker.py
+
+which is under the MIT license.
+"""
+import hashlib
+import hmac
+import json
+import os
+
+import git
+from flask import abort, request
+
+from .api import api
+
+w_secret = os.environ["WEBHOOK_SECRET"]
+
+
+def is_valid_signature(x_hub_signature, data, private_key):
+    hash_algorithm, github_signature = x_hub_signature.split("=", 1)
+    algorithm = hashlib.__dict__.get(hash_algorithm)
+    encoded_key = bytes(private_key, "latin-1")
+    mac = hmac.new(encoded_key, msg=data, digestmod=algorithm)
+    return hmac.compare_digest(mac.hexdigest(), github_signature)
+
+
+@api.route("/update_server", methods=["POST"])
+def webhook():
+    if request.method != "POST":
+        return "OK"
+    else:
+        abort_code = 418
+        # Do initial validations on required headers
+        if "X-Github-Event" not in request.headers:
+            abort(abort_code)
+        if "X-Github-Delivery" not in request.headers:
+            abort(abort_code)
+        if "X-Hub-Signature" not in request.headers:
+            abort(abort_code)
+        if not request.is_json:
+            abort(abort_code)
+        if "User-Agent" not in request.headers:
+            abort(abort_code)
+        ua = request.headers.get("User-Agent")
+        if not ua.startswith("GitHub-Hookshot/"):
+            abort(abort_code)
+
+        event = request.headers.get("X-GitHub-Event")
+        if event == "ping":
+            return json.dumps({"msg": "Hi!"})
+        if event != "push":
+            return json.dumps({"msg": "Wrong event type"})
+
+        x_hub_signature = request.headers.get("X-Hub-Signature")
+        # webhook content type should be application/json for request.data to have the payload
+        # request.data is empty in case of x-www-form-urlencoded
+        if not is_valid_signature(x_hub_signature, request.data, w_secret):
+            print("Deploy signature failed: {sig}".format(sig=x_hub_signature))
+            abort(abort_code)
+
+        payload = request.get_json()
+        if payload is None:
+            print("Deploy payload is empty: {payload}".format(payload=payload))
+            abort(abort_code)
+
+        if payload["ref"] != "refs/heads/master":
+            return json.dumps({"msg": "Not master; ignoring"})
+
+        repo = git.Repo("/var/www/sites/mysite")
+        origin = repo.remotes.origin
+
+        pull_info = origin.pull()
+
+        if len(pull_info) == 0:
+            return json.dumps({"msg": "Didn't pull any information from remote!"})
+        if pull_info[0].flags > 128:
+            return json.dumps({"msg": "Didn't pull any information from remote!"})
+
+        commit_hash = pull_info[0].commit.hexsha
+        build_commit = f'build_commit = "{commit_hash}"'
+        print(f"{build_commit}")
+        return "Updated PythonAnywhere server to commit {commit}".format(
+            commit=commit_hash
+        )
