97986909c2ca905fc8489619b61adfca67ee8dc0
John Maximilian <2e0byo@gmail.com>
2021-02-03 21:21:26 +0000

    parse range

--- backend/bible/bible.py
+++ backend/bible/bible.py
@@ -1,3 +1,5 @@
+import re
+
 from dotmap import DotMap
 
 
@@ -5,3 +7,10 @@ class Bible:
     def __init__(self, version: str):
         self.version = version
         self.content = DotMap()
+
+    def _parse_ref(self, ref: str):
+        """Convert ref into book, chapter and verse."""
+        book, chapter, verse = re.search(r"([0-9]*.+?)([0-9]+):([0-9]+)", ref).groups()
+        chapter = int(chapter)
+        verse = int(verse)
+        return book, chapter, verse
